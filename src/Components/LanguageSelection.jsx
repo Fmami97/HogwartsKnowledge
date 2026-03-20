fb69a8d32c414b8cdda15f27a0eee649128d90fe
fmami <francesco.mami@hes-so.ch>
2026-03-20 09:58:56 +0100

    changed location of the language files to public, in order to be accessible through production build

--- src/Components/LanguageSelection.jsx
+++ src/Components/LanguageSelection.jsx
@@ -7,7 +7,7 @@ export default function LanguageSelection({language,setLanguage,languages}) {
             {languages.map(lang => (
                 //shows a button with the icon of the language and the name of the language, when clicked it changes the language of the app.
                 <button key={lang} className={`language-button ${language === lang ? "active" : ""}`} onClick={() => setLanguage(lang)}>
-                    <img src={"./src/assets/"+lang+".png"}  alt={lang.name} className="language-icon" />
+                    <img src={"./assets/"+lang+".png"}  alt={lang.name} className="language-icon" />
                     <span>{lang.name}</span>
                 </button>
             ))}
